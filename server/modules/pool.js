9b436cb632ba876abbf46b5704966617c4f89a75
Myron <dubtwee@gmail.com>
2019-12-02 09:41:51 -0600

    creating pool to connect to DB

---
+++ server/modules/pool.js
@@ -0,0 +1,27 @@
+const pg = require('pg');
+
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    config = {}
+} else {
+    config = {
+        host: 'localhost',
+        port: 5432,
+        database: 'weekend9-to-do-list',
+        max: 10,
+        idleTimeoutMillis: 30000,
+    }
+}
+
+const pool = new pg.Pool(config);
+
+pool.on('connect', () => {
+    console.log('Pool is connected!');
+});
+
+pool.on('error', () => {
+    console.log('Error in Pool, get cleaner.');
+});
+
+module.exports = pool;

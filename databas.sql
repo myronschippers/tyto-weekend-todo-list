1773cd65bfe5e2db4a87e9e0af0d7a53fd509718
Myron <dubtwee@gmail.com>
2019-12-02 09:14:21 -0600

    insert statments for initial data

--- databas.sql
+++ databas.sql
@@ -3,3 +3,9 @@ CREATE TABLE "todo" (
     task VARCHAR(120),
     complete BOOLEAN
 );
+
+INSERT INTO "todo" ("task", "complete")
+VALUES ('feed cat', 'True');
+
+INSERT INTO "todo" ("task", "complete")
+VALUES ('stitch foot', 'True');

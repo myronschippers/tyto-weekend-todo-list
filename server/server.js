4a83935ccbcce9b72cddae0222792734c43791ea
Myron <dubtwee@gmail.com>
2019-12-02 09:47:37 -0600

    stubbing in api routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pool = require('./modules/pool');
 
 const app = express();
 const PORT = 5000;
@@ -7,6 +8,15 @@ const PORT = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
+
+// CREATE TODO GET
+app.get('/api/todo', (req, res) => {});
+
+// CREATE TODO POST
+app.post('/api/todo', (req, res) => {});
+
+// CREATE TODO PUT
+app.put('/api/todo', (req, res) => {});
 
 app.listen(PORT, () => {
     console.log('Server running on ', PORT);

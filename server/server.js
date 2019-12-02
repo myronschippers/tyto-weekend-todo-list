73aa6bc99092867174de92ca329ea81dd1bcecc7
Myron <dubtwee@gmail.com>
2019-12-02 09:29:39 -0600

    setup for body-parser

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
